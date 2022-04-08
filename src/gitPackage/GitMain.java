b490a2737a54626a226c2688d09dc6ca6c8f7ad1
rafa <rafaeljoseossoriolopez@gmail.com>
2022-04-08 16:25:43 +0200

    v1.2

--- src/gitPackage/GitMain.java
+++ src/gitPackage/GitMain.java
@@ -9,4 +9,8 @@ public class GitMain {
 	public static void saludoAlumno() {
 		System.out.println("Hola, soy Rafa. Este método viene de la rama saludoAlumno.");
 	}
+	
+	public static void despedidaAlumno() {
+		System.out.println("Este método viene de la rama despedidaAlumno. ¡Pasa una buena Semana Santa!");
+	}
 }
